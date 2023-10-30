859fa1951b447f4cc85a58ff0fd7a1842a8569f8
txquang2202 <tranxuanquang81@gmail.com>
2023-10-30 14:03:17 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
+function FilterableImageLoader({ images }) {
+  const [filterText, setFilterText] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SearchBar filterText={filterText} onFilterTextChange={setFilterText} />
+      <Images images={images} filterText={filterText} />
     </div>
   );
 }
+function Images({ filterText }) {
+  const [images, setImages] = useState([]);
+  const YOUR_ACCESS_KEY = "eupMYpd7d9Rbvo4SbiQWpOP7dTGyOVv4P19tLFVGfgU";
+  const toggle = true;
+  useEffect(() => {
+    async function fetchUnsplashImages() {
+      try {
+        const response = await fetch(
+          `https://api.unsplash.com/photos/?client_id=${YOUR_ACCESS_KEY}&query=random&count=10  `
+        );
 
-export default App;
+        if (!response.ok) {
+          throw new Error("Network error");
+        }
+
+        const data = await response.json();
+        setImages(data);
+      } catch (error) {
+        console.error("Fetch error:", error);
+      }
+    }
+    fetchUnsplashImages();
+  }, []);
+
+  const filteredImages = images.filter((image) =>
+    image.slug.toLowerCase().includes(filterText.toLowerCase())
+  );
+
+  return (
+    <div>
+      {toggle ? (
+        <div className="image-container">
+          {filteredImages.map((image, index) => (
+            <img
+              key={index}
+              src={image.urls.regular}
+              alt={image.slug}
+              className="image"
+            />
+          ))}
+        </div>
+      ) : (
+        <div>Đéo có kết quả</div>
+      )}
+    </div>
+  );
+}
+function SearchBar({ filterText, onFilterTextChange }) {
+  return (
+    <form>
+      <input
+        type="text"
+        value={filterText}
+        placeholder="Search..."
+        onChange={(e) => onFilterTextChange(e.target.value)}
+      />
+    </form>
+  );
+}
+export default function App() {
+  return <FilterableImageLoader />;
+}
