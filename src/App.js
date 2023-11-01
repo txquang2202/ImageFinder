3f7cd411ec4ac23e3f3f4aeb161b1eedaea5421f
txquang2202 <tranxuanquang81@gmail.com>
2023-11-01 14:11:44 +0700

    Final

--- src/App.js
+++ src/App.js
@@ -1,58 +1,66 @@
 import { useState, useEffect, useRef } from "react";
 
-function FilterableImageLoader({ images }) {
-  const [filterText, setFilterText] = useState("");
+function SearchBar({ onSearch }) {
+  const [query, setQuery] = useState("random");
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    onSearch(query);
+    setQuery("");
+  };
 
   return (
-    <div>
-      <SearchBar filterText={filterText} onFilterTextChange={setFilterText} />
-      <Images images={images} filterText={filterText} />
-    </div>
+    <form onSubmit={handleSearchSubmit} className="search">
+      <input
+        type="text"
+        value={query}
+        placeholder="Search..."
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button type="submit" class="searchButton">
+        Search
+      </button>
+    </form>
   );
 }
 
-function Images({ filterText }) {
+function Images({ queryText, key }) {
   const [images, setImages] = useState([]);
-  const YOUR_ACCESS_KEY = "eupMYpd7d9Rbvo4SbiQWpOP7dTGyOVv4P19tLFVGfgU";
+  const YOUR_ACCESS_KEY = "-toNEvxV5hfS1hX3aNk6doP46p1L1eTqU3rj7VXxBg8";
   const page = useRef(1);
   const loading = useRef(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const loadMoreImages = async () => {
-    if (loading.current) return;
-
+    const query = queryText;
+    if (loading.current || isLoading) return;
+    console.log(query);
     loading.current = true;
+    setIsLoading(true);
 
     try {
       const response = await fetch(
-        `https://api.unsplash.com/photos/?client_id=${YOUR_ACCESS_KEY}&page=${page.current}&count=10`
+        `https://api.unsplash.com/search/photos/?client_id=${YOUR_ACCESS_KEY}&page=${page.current}&query=${query}`
       );
 
-      if (!response.ok) {
-        throw new Error("Network error");
-      }
-
       const data = await response.json();
-      setImages((prevImages) => [...prevImages, ...data]);
+      setImages((prevImages) => [...prevImages, ...data.results]);
       page.current++;
     } catch (error) {
       console.error("Fetch error:", error);
     } finally {
       loading.current = false;
+      setIsLoading(false);
     }
   };
 
   useEffect(() => {
     loadMoreImages();
-  }, []);
-
-  const filteredImages = images.filter((image) =>
-    image.slug.toLowerCase().includes(filterText.toLowerCase())
-  );
-
+  }, [queryText, key]);
   const handleScroll = () => {
     if (
-      window.innerHeight + document.documentElement.scrollTop ===
-      document.documentElement.offsetHeight
+      window.innerHeight + document.documentElement.scrollTop >=
+      document.documentElement.offsetHeight - 100
     ) {
       loadMoreImages();
     }
@@ -68,7 +76,7 @@ function Images({ filterText }) {
   return (
     <div>
       <div className="image-container">
-        {filteredImages.map((image, index) => (
+        {images.map((image, index) => (
           <img
             key={index}
             src={image.urls.regular}
@@ -77,22 +85,32 @@ function Images({ filterText }) {
           />
         ))}
       </div>
+      {isLoading && <div className="loading">LOADING.....</div>}
     </div>
   );
 }
 
-function SearchBar({ filterText, onFilterTextChange }) {
+function FilterableImageLoader({ images }) {
+  const [query, setQuery] = useState("random");
+  const [key, setKey] = useState(0);
+
+  const handleSearch = (newQuery) => {
+    setQuery(newQuery);
+    setKey((prevKey) => prevKey + 1);
+  };
+
   return (
-    <form>
-      <input
-        type="text"
-        value={filterText}
-        placeholder="Search..."
-        onChange={(e) => onFilterTextChange(e.target.value)}
-      />
-    </form>
+    <div>
+      <div className="container">
+        <SearchBar query={query} onSearch={handleSearch} />
+        <div className="list-images">
+          <Images images={images} queryText={query} key={key} />
+        </div>
+      </div>
+    </div>
   );
 }
+
 export default function App() {
   return <FilterableImageLoader />;
 }
